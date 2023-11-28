c36d013d30b1669028b8c6f30eb8e9e178a21805
litpu <litput0@gmail.com>
2023-11-29 00:58:04 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Ex1
+{
+    static void Main()
+    {
+        string filePath = @"C:\Users\litpu\OneDrive\Desktop\Words.txt";
+
+        string[] words = File.ReadAllLines(filePath);
+
+        if (words.Length < 3)
+        {
+            Console.WriteLine("В файле должно быть как минимум 3 слова");
+            return;
+        }
+
+        string password = GeneratePassword(words);
+        
+        Console.WriteLine("Полученный пароль: " + password);
+    }
+
+    static string GeneratePassword(string[] words)
+    {
+        Random random = new Random();
+        
+        string[] selectedWords = words.OrderBy(w => random.Next()).Take(random.Next(3, 5)).ToArray();
+        
+        string password = string.Join("", selectedWords.Select(w => w.Substring(0, Math.Min(3, w.Length))));
+
+        return password;
+    }
+}
