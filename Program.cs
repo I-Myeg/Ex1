cd688860cd772e284d8fa51e1361c8f6a8e8bb29
litpu <litput0@gmail.com>
2023-11-30 17:43:45 +0300

    add ConvertToEnglishLayout function

--- Program.cs
+++ Program.cs
@@ -13,8 +13,12 @@
         }
 
         string password = GeneratePassword(words);
+
+        string EngPassword = ConvertToEnglishLayout(password);
         
-        Console.WriteLine("Полученный пароль: " + password);
+        Console.WriteLine("Полученный пароль в русской раскладке: " + password);
+        
+        Console.WriteLine("Полученный пароль в английской раскладке: " + EngPassword);
     }
 
     static string GeneratePassword(string[] words)
@@ -27,4 +31,22 @@
 
         return password;
     }
+
+    static string ConvertToEnglishLayout(string password)
+    {
+        Dictionary<char, char> layoutMapping = new Dictionary<char, char>
+        {
+            { 'а', 'f' }, { 'б', ',' }, { 'в', 'd' }, { 'г', 'u' }, { 'д', 'l' },
+            { 'е', 't' }, { 'ё', '`' }, { 'ж', ';' }, { 'з', 'p' }, { 'и', 'b' },
+            { 'й', 'q' }, { 'к', 'r' }, { 'л', 'k' }, { 'м', 'v' }, { 'н', 'y' },
+            { 'о', 'j' }, { 'п', 'g' }, { 'р', 'h' }, { 'с', 'c' }, { 'т', 'n' },
+            { 'у', 'e' }, { 'ф', 'a' }, { 'х', '[' }, { 'ц', 'w' }, { 'ч', 'x' },
+            { 'ш', 'i' }, { 'щ', 'o' }, { 'ъ', 'm' }, { 'ы', 's' }, { 'ь', '\'' },
+            { 'э', ']' }, { 'ю', '.' }, { 'я', 'z' }, { ' ', ' ' }
+        };
+
+        return new string(password.ToLower().ToCharArray().Select(c =>
+            layoutMapping.ContainsKey(c) ? layoutMapping[c] : c).ToArray());
+    }
+    
 }
